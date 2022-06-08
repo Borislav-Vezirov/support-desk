9a2cdb5f6d2419d63b515b49f116b64c062f4ea3
Borislav Vezirov <bobi.vez@gmail.com>
2022-06-08 20:41:26 +0300

    removed some files and added header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+
+function Header(){
+
+    return(
+        <header>
+        <nav>
+           <p>Support Desk</p>
+           <form>
+            <input type="text" placeholder="Search" />
+           </form>
+        </nav>
+    </header>   
+    )
+}
+
+export default Header;
